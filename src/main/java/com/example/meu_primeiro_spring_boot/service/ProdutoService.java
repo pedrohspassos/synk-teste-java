561c3d26ae5ed3fe1a023255cf84d89b6ffcde6f
Pedro Henrique Passos <pedro.hspassos19@gmail.com>
2025-09-09 21:42:00 -0300

    Adicionando arquivo

--- src/main/java/com/example/meu_primeiro_spring_boot/service/ProdutoService.java
+++ src/main/java/com/example/meu_primeiro_spring_boot/service/ProdutoService.java
@@ -9,3 +9,35 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+@Service
+public class ProdutoService {
+    private final ProdutoRepository produtoRepository;
+
+    public ProdutoService(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
+    public List<Produto> listarProdutos(){
+
+        return produtoRepository.findAll();
+    }
+
+    public Produto buscarProdutoPorId(Long id){
+        return produtoRepository.findById(id)
+                .orElseThrow(() -> new RecursoNaoEncontradoException("Produto com ID " + id + " não encontrado"));
+    }
+
+    public Produto salvarProduto(Produto produto) {
+        return produtoRepository.save(produto);
+    }
+
+    public void excluirProduto(Long id){
+
+        if(!produtoRepository.existsById(id)){
+            throw new RecursoNaoEncontradoException("Produto com ID " + id + " não encontrado");
+        }
+
+        produtoRepository.deleteById(id);
+    }
+
+}
